a9cbdab6fb900d6ba603d459de2efa42d4ad66de
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-04-08 21:44:04 +0530

    python env removed

--- img.py
+++ img.py
@@ -1,7 +1,6 @@
 import os
 import requests
-from googlesearch import search_images
-
+from google_images_search import GoogleImagesSearch
 
 def download_image(url, save_path):
     response = requests.get(url, stream=True)
@@ -10,24 +9,28 @@ def download_image(url, save_path):
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-
 def search_and_download_images(query, num_images=10, target_directory='images'):
     os.makedirs(target_directory, exist_ok=True)
 
-    for i, image_url in enumerate(search_images(query, num_results=num_images)):
+    gis = GoogleImagesSearch('<your_google_developers_api_key>', '<your_google_custom_search_engine_id>')
+    search_params = {
+        'q': query,
+        'num': num_images,
+    }
+
+    gis.search(search_params)
+
+    for i, image in enumerate(gis.results()):
         try:
-            save_path = os.path.join(
-                target_directory, f"{query.replace(' ', '_')}_{i}.jpg")
-            download_image(image_url, save_path)
+            save_path = os.path.join(target_directory, f"{query.replace(' ', '_')}_{i}.jpg")
+            download_image(image.url, save_path)
             print(f"Downloaded {save_path}")
         except Exception as e:
-            print(f"Failed to download {image_url}: {e}")
-
+            print(f"Failed to download {image.url}: {e}")
 
 if __name__ == "__main__":
-    celebrity_name = "Angelina Jolie"
-    num_images = 10
-
-    target_directory = "celebrity_images"
+    celebrity_name = "Angelina Jolie"  # Replace with your desired celebrity name
+    num_images = 10  # Adjust the number of images to download
+    target_directory = "celebrity_images"  # Set the target directory for saving images
 
     search_and_download_images(celebrity_name, num_images, target_directory)
